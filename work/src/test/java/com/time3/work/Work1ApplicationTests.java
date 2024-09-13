0b4c2f64fbd631452bbf64b3bfb2e9a28f4313fa
Poteter <eith2312@gmail.com>
2024-09-13 09:46:20 +0200

    added empty springboot project

---
+++ work/src/test/java/com/time3/work/Work1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.time3.work;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Work1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
